e438526042e0309e7e9f2e4d516f71e646141386
alikumbhar <ali_kumbhar@hotmail.com>
2025-03-24 16:44:03 +0500

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def call(String URL, String brance){
+def call(String URL, String branch){
   echo "code clonning Started"
   git url: "${URL}", branch:"${branch}"
   echo "code Successfully clonned"
